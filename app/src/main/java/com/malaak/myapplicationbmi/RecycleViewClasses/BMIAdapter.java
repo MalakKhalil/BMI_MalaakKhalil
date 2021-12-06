815f072481c9551889ebb9295a1f8d93fa4ce404
MalaakKhalil <malaak31999@gmail.com>
2021-12-06 09:15:20 -0800

    BMImalaakKhalil4

--- app/src/main/java/com/malaak/myapplicationbmi/RecycleViewClasses/BMIAdapter.java
+++ app/src/main/java/com/malaak/myapplicationbmi/RecycleViewClasses/BMIAdapter.java
@@ -1,13 +1,23 @@
 package com.malaak.myapplicationbmi.RecycleViewClasses;
 
+import android.content.Context;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class BMIAdapter extends RecyclerView.Adapter<BMIHolder> {
+    private User u;
+    private Context c;
+
+    public BMIAdapter(User u, Context c) {
+        this.u = u;
+        this.c = c;
+    }
+
     @NonNull
     @Override
+
     public BMIHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
--- app/src/main/java/com/malaak/myapplicationbmi/RecycleViewClasses/BMIHolder.java
+++ app/src/main/java/com/malaak/myapplicationbmi/RecycleViewClasses/BMIHolder.java
@@ -1,13 +1,16 @@
 package com.malaak.myapplicationbmi.RecycleViewClasses;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class BMIHolder extends RecyclerView.ViewHolder {
+private View itemView;
+
     public BMIHolder(@NonNull View itemView) {
         super(itemView);
-
+        this.itemView=itemView;
     }
 }
