d0bbcb92fda81faa36d09eece05cc066800e4622
MalaakKhalil <malaak31999@gmail.com>
2021-12-06 07:00:24 -0800

    BMImalaakKhalil2

--- app/src/main/java/com/malaak/myapplicationbmi/Login.java
+++ app/src/main/java/com/malaak/myapplicationbmi/Login.java
@@ -2,13 +2,35 @@ package com.malaak.myapplicationbmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
-    @Override
+    EditText Username ;
+    EditText password;
+    Button Login;
+    TextView SignUp;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Username=findViewById(R.id.Username);
+        password=findViewById(R.id.passwordinlogin);
+        Login=findViewById(R.id.log_in_btn);
+        SignUp=findViewById(R.id.sign_up);
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Login.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
-}
+
+    }
